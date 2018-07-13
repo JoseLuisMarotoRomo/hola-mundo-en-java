f6a847324ea5b317459771e821d5a989a12f67d1
Jose Luis Maroto Romo <jlmaroto@centrored.net>
2018-07-13 14:20:20 +0200

    comentarios añadidos al código

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,8 @@
+/**
+ * Muestra por pantalla la frase "¡Hola Mundo!"
+ *
+ * @author Jose Luis 
+ */
 public class HolaMundo{
 	public static void main(String[] args) {
 		System.out.println("¡Hola Mundo!");
